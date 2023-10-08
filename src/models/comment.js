a4aff04c1cb34180b9f01b336ec1dc6ee5c6a511
Akshay Kamble <kamblea635@gmail.com>
2023-10-08 12:27:45 +0530

    update: comment model with refpath

--- src/models/comment.js
+++ src/models/comment.js
@@ -3,11 +3,16 @@ const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema(
   {
     content: { type: String, required: true },
-    email: { type: String },
-    tweetId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Comment",
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    onModel: {
+      type: String,
       required: true,
+      enum: ["Tweet", "Comment"],
+    },
+    commentable: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      refPath: "onModel",
     },
   },
   {
